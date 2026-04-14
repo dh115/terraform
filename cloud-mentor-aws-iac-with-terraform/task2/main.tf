eae363645ed1f3f47d58d9c4fd55390b0af39b5c
dh115 <david.harutyunyan115@gmail.com>
2026-04-14 10:08:57 +0400

    Fic

--- cloud-mentor-aws-iac-with-terraform/task2/main.tf
+++ cloud-mentor-aws-iac-with-terraform/task2/main.tf
@@ -2,35 +2,3 @@
 provider "aws" {
   region = var.aws_region
 }
-
-# Reference the pre-existing VPC created by the course platform via CloudFormation.
-# We use a data source because Terraform does not own this resource.
-data "aws_vpc" "main" {
-  filter {
-    name   = "tag:Name"
-    values = ["${var.project_id}-vpc"]
-  }
-}
-
-# Discover public subnets within the VPC.
-# Filtered by map-public-ip-on-launch=true to ensure EC2 instances get a public IP.
-data "aws_subnets" "public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.main.id]
-  }
-
-  filter {
-    name   = "map-public-ip-on-launch"
-    values = ["true"]
-  }
-}
-
-# Reference the pre-existing security group created by the course platform.
-# This SG allows inbound SSH (port 22) access to EC2 instances.
-data "aws_security_group" "ssh" {
-  filter {
-    name   = "tag:Name"
-    values = [var.aws_security_group_name]
-  }
-}
